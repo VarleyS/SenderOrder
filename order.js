e69c4dd4af372c41d34a9e063311c5d2b423bd61
VarleyS <varleysr@gmail.com>
2022-11-17 10:15:28 -0300

    ajustes de css

--- order.js
+++ order.js
@@ -1,11 +1,11 @@
 $('.order').click(function(e) {
     let button = $(this);
 
-    if(!button.hasClass('animate')) {
+    if (!button.hasClass('animate')) {
         button.addClass('animate');
 
-        setInterval(() => {
+        setTimeout(() => {
             button.removeClass('animate');
         }, 10000);
     }
-});
+}); 
